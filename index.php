f9e90d405a66ce9f561455d3df1acb0f939df59f
Marc Zijderveld <marczijderveld@planet.nl>
2017-04-01 19:16:04 +0200

    Index endpoints

--- index.php
+++ index.php
@@ -1,10 +1,8 @@
 Visit the below endpoints<br/>
 
-Login:<br/>
-http://YOUR_MACHINE_IP/android_login_api/login.php<br/><br/>
+Appartment:<br/>
+        http://<school_adres>/api/appartment/<typeid><br/><br/>
 
-Registration:<br/>
-http://YOUR_MACHINE_IP/android_login_api/register.php<br/><br/>
+Images:<br/>
+        http://<school_adres>/api/images/<typeid><br/><br/>
 
-Tutorial link:<br/>
-<a href="http://www.androidhive.info/2012/01/android-login-and-registration-with-php-mysql-and-sqlite/">http://www.androidhive.info/2012/01/android-login-and-registration-with-php-mysql-and-sqlite/</a>
