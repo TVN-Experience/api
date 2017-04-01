0238dcbb9c206bc63db7f8a5b0df773a594875fa
Marc Zijderveld <marczijderveld@planet.nl>
2017-04-01 19:25:34 +0200

    Endpoint reference tweaks

--- index.php
+++ index.php
@@ -1,8 +1,8 @@
 Visit the below endpoints<br/>
 
 Appartment:<br/>
-        http://<school_adres>/api/appartment/<typeid><br/><br/>
+https://project.cmi.hr.nl/2016_2017/bcp_mt3b_t2/api/appartment/$type_id<br/><br/>
 
 Images:<br/>
-        http://<school_adres>/api/images/<typeid><br/><br/>
+https://project.cmi.hr.nl/2016_2017/bcp_mt3b_t2/api/images/$type_id<br/><br/>
 
